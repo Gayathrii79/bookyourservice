abc295c00cdc2ee692d9214dc8be3c08d844382b
Gayathrii_79 <gayathrim864@gmail.com>
2026-08-12 18:22:51 +0530

    Refresh favicon

--- bys_UI/src/routes/__root.tsx
+++ bys_UI/src/routes/__root.tsx
@@ -113,7 +113,7 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
     ],
     links: [
       { rel: "stylesheet", href: appCss },
-      { rel: "icon", href: "/favicon.png", type: "image/png" },
+      { rel: "icon", href: "/favicon.png?v=2", type: "image/png" },
       { rel: "preconnect", href: "https://fonts.googleapis.com" },
       { rel: "preconnect", href: "https://fonts.gstatic.com", crossOrigin: "anonymous" },
       {
